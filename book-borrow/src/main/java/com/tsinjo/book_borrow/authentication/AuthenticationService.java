8c83ce900dd0d29dec5f2221a3e925734dc1682a
TsinjoNantosoa <tsinjonantosoa@gmail.com>
2025-04-09 05:56:24 +0300

    modification

--- book-borrow/src/main/java/com/tsinjo/book_borrow/authentication/AuthenticationService.java
+++ book-borrow/src/main/java/com/tsinjo/book_borrow/authentication/AuthenticationService.java
@@ -2,9 +2,12 @@ package com.tsinjo.book_borrow.authentication;
 
 import com.tsinjo.book_borrow.role.RoleRepository;
 import com.tsinjo.book_borrow.user.User;
+import com.tsinjo.book_borrow.user.UserRepository;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.security.SecureRandom;
 
 import java.util.List;
 
@@ -12,6 +15,9 @@ import java.util.List;
 @RequiredArgsConstructor
 @Builder
 public class AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
     private RoleRepository roleRepository;
     public void register(RegistrationRequest request) {
         var userRole = roleRepository.findByName("USER")
@@ -20,13 +26,39 @@ public class AuthenticationService {
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())
                 .email(request.getPassword())
-                .password("")
+                .password(passwordEncoder.encode(request.getPassword()))
                 .accountLocked(false)
                 .enable(false)
                 .roles(List.of(userRole))
                 .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+
 
     }
 
+    private void sendValidationEmail(User user) {
+        var newToken =generateAndSaveActivationToken(user);
+        //send the email
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        //generate the token
+        String generateToken = generateAndSaveActivationCode(6);
+        return null;
+    }
+
+
+
+    private String generateAndSaveActivationCode(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        SecureRandom secureRandom = new SecureRandom();
+        StringBuilder activationCode = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            activationCode.append(characters.charAt(randomIndex));
+        }
+        return activationCode.toString();
+    }
 }
 
