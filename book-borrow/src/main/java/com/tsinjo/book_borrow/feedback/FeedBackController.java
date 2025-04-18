1adcce5c12ac860929e2665971ab3f3c0fc34374
TsinjoNantosoa <tsinjonantosoa@gmail.com>
2025-04-18 03:50:06 +0300

    end of backend modification

--- book-borrow/src/main/java/com/tsinjo/book_borrow/feedback/FeedBackController.java
+++ book-borrow/src/main/java/com/tsinjo/book_borrow/feedback/FeedBackController.java
@@ -1,41 +1,41 @@
-//package com.tsinjo.book_borrow.feedback;
-//
-//import com.tsinjo.book_borrow.book.PageResponse;
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//import jakarta.validation.Valid;
-//import lombok.RequiredArgsConstructor;
-//import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("feedbacks")
-//@RequiredArgsConstructor
-//@Tag(name = "Feedback")
-//public class FeedBackController {
-//
-//    private final  FeedBackService service;
-//
-//    @PostMapping("")
-//    public ResponseEntity<Integer> saveFeedBack(
-//            @Valid @RequestBody FeedBackRequest request,
-//            Authentication connectedUser
-//    ){
-//        return ResponseEntity.ok(service.save(request, connectedUser));
-//    }
-//
-//    @GetMapping("/book/{book-id}")
-//    public ResponseEntity<PageResponse<FeedBackResponse>> findAllFeedBackByBook(
-//            @PathVariable("book-id") Integer bookId,
-//            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
-//            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
-//            Authentication connectedUser
-//
-//    ){
-//        return ResponseEntity.ok(service.findAllFeedBackByBook(bookId,page,size,connectedUser));
-//    }
-//
-//
-//
-//
-//}
+package com.tsinjo.book_borrow.feedback;
+
+import com.tsinjo.book_borrow.book.PageResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("feedbacks")
+@RequiredArgsConstructor
+@Tag(name = "Feedback")
+public class FeedBackController {
+
+    private final  FeedBackService service;
+
+    @PostMapping("")
+    public ResponseEntity<Integer> saveFeedBack(
+            @Valid @RequestBody FeedBackRequest request,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.save(request, connectedUser));
+    }
+
+    @GetMapping("/book/{book-id}")
+    public ResponseEntity<PageResponse<FeedBackResponse>> findAllFeedBackByBook(
+            @PathVariable("book-id") Integer bookId,
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+
+    ){
+        return ResponseEntity.ok(service.findAllFeedBackByBook(bookId,page,size,connectedUser));
+    }
+
+
+
+
+}
